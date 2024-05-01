689c08f1ef94878c6c325ce968bb6bda1709b602
Ziting Zhao <Cocomeri17@gmail.com>
2024-05-01 13:23:27 +0100

    test runs

--- CodePrep-admin/src/main/java/com/CodePrep/CodePrepAdminApplication.java
+++ CodePrep-admin/src/main/java/com/CodePrep/CodePrepAdminApplication.java
@@ -1,12 +1,13 @@
 package com.CodePrep;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
-@SpringBootApplication(scanBasePackages = {"com.CodePrep"})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableTransactionManagement
 @EnableScheduling
 @EnableAsync
